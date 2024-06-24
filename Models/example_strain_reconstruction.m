eefe5d5c4bcb992651b1df37faf9421f7c00423e
mneal98 <mneal@ucsd.edu>
2024-06-24 10:33:23 -0700

    Add files via upload

--- Models/example_strain_reconstruction.m
+++ Models/example_strain_reconstruction.m
@@ -6,10 +6,12 @@ strain_list=importdata('strain_list.mat');
 %strain association matrix, then remove the rxns associated with zeros in
 %this binary vector.
 
-%suppose you are interested in strain TH016, linked to identifier
-%GCF_016411905_1:
-idx=find(strcmp(strain_list,'GCF_016411905_1'));
+%suppose you are interested in strain 30VD-1, linked to identifier
+%GCF_004366695_1:
+idx=find(strcmp(strain_list,'GCF_004366695_1'));
 rxn_presence=rxn_strain_matrix(:,idx);
 absent=rxn_presence==0;
-TH016_model=removeRxns(pan_model,pan_model.rxns(absent));
+model_30VD1=removeRxns(pan_model,pan_model.rxns(absent));
 
+%confirm that it runs:
+FBA=optimizeCbModel(model_30VD1)
